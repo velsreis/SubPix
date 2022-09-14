8643df0c22e7d9207c8be13f4ec411cdab94452a
velsreis <78064698+velsreis@users.noreply.github.com>
2022-09-14 12:31:56 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+
+import tmi from 'tmi.js';
+
+const client = new tmi.Client({
+	channels: [ 'my_name' ]
+});
+
+client.connect();
+
+client.on('message', (channel, tags, message, self) => {
+	// "Alca: Hello, World!"
+	console.log(`${tags['display-name']}: ${message}`);
+});
+					
